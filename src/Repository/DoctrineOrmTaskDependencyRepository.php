97d5c4461c65d97119d42df322658bef4c058d8f
Héctor Franco <hectorfranco@nowo.tech>
2026-07-06 23:52:28 +0200

    fix: add EntityManager constructor to empty Doctrine repository stubs
    Fixes Symfony DI errors when running TimeTrackBundle demo with path-mounted TaskBoardBundle.

--- src/Repository/DoctrineOrmTaskDependencyRepository.php
+++ src/Repository/DoctrineOrmTaskDependencyRepository.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Nowo\TaskBoardBundle\Repository;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 final readonly class DoctrineOrmTaskDependencyRepository implements TaskDependencyRepositoryInterface
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+    ) {
+    }
 }
--- src/Repository/DoctrineOrmTaskTimeEntryRepository.php
+++ src/Repository/DoctrineOrmTaskTimeEntryRepository.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Nowo\TaskBoardBundle\Repository;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 final readonly class DoctrineOrmTaskTimeEntryRepository implements TaskTimeEntryRepositoryInterface
 {
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+    ) {
+    }
 }
